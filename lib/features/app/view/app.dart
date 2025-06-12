0da7c2f576dcece007c68f173c2685eb96153151
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2025-06-11 21:35:35 -0500

    chore: empty username view

---
+++ lib/features/app/view/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fractal_onboarding_poc/features/username/view/username_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AppBody();
+  }
+}
+
+class AppBody extends StatelessWidget {
+  const AppBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Face Validator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const UsernameView(),
+    );
+  }
+}
---
+++ lib/features/username/view/username_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UsernameView extends StatelessWidget {
+  const UsernameView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const UsernameBody();
+  }
+}
+
+class UsernameBody extends StatelessWidget {
+  const UsernameBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
